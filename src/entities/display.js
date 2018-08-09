fcdbe0047f957c92ff726a5fe6ce0edc072e5a84
Nick Girardo <nickgirardo@gmail.com>
2018-08-09 17:14:11 -0400

    Perf: only redraw displays when needed

--- src/entities/display.js
+++ src/entities/display.js
@@ -5,6 +5,7 @@ export default class TetrominoDisplay {
 
   constructor(type) {
     this.isEmpty = false;
+    this.redraw = true;
 
     this.width = 4;
     this.height = 4;
@@ -26,10 +27,15 @@ export default class TetrominoDisplay {
   }
 
   draw(domGrid, gridWidth, offsetX=0, offsetY=0) {
+    if(!this.redraw)
+      return;
+
+    this.redraw = false;
     // First draw empty background area to display tetromino within
     for(let i=0; i<this.height; i++) {
       for(let j=0; j<this.width; j++) {
-        domGrid[(i+offsetY)*gridWidth + offsetX + j].className = this.classNameBg;
+        const target = (i+offsetY)*gridWidth + offsetX + j;
+        domGrid[target].className = this.classNameBg;
       }
     }
 
--- src/entities/emptyDisplay.js
+++ src/entities/emptyDisplay.js
@@ -3,6 +3,7 @@ export default class EmptyDisplay {
 
   constructor() {
     this.isEmpty = true;
+    this.redraw = true;
 
     this.width = 4;
     this.height = 4;
@@ -10,6 +11,10 @@ export default class EmptyDisplay {
   }
 
   draw(domGrid, gridWidth, offsetX=0, offsetY=0) {
+    if(!this.redraw)
+      return;
+
+    this.redraw = false;
     // Only need to draw empty bg
     for(let i=0; i<this.height; i++) {
       for(let j=0; j<this.width; j++) {
