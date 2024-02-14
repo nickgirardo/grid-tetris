f65ec1cc61f0e32b934b413584409be7e0020f2d
Nick Girardo <nickgirardo@gmail.com>
2024-02-13 21:38:32 -0500

    Update build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/app.js',
-  mode: 'development',
-  devtool: 'source-map',
+  entry: "./src/app.js",
+  mode: "development",
+  devtool: "source-map",
   output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js',
+    path: __dirname + "/dist",
+    filename: "bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CopyWebpackPlugin({ patterns: [{ from: "static" }] }),
+  ],
 };
